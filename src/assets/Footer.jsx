0d947f583c7008916f05317995453c661a36a05a
John Kenneth <johnkennethagpaoa@gmail.com>
2023-06-02 20:32:32 +0800

    add header, footer and note components

---
+++ src/assets/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    const year = new Date().getFullYear()
+    return (
+        <footer>
+            <p>Copyright &#169; {year}</p>
+        </footer>
+    )
+}
+
+export default Footer
